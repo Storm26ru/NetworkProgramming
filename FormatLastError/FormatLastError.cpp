643c51646ab475d95509eafec6032fb41f5a3a66
Storm <Storm@STORMSSD>
2025-06-17 00:07:27 +0300

    creation clientsocket done

---
+++ FormatLastError/FormatLastError.cpp
@@ -0,0 +1,22 @@
+#include"FormatLastError.h"
+
+LPSTR FormatLastError(DWORD dwMessageID)
+{
+	LPSTR szBuffer = NULL;
+	FormatMessage
+	(			  FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+				  NULL,
+				  dwMessageID,
+				  MAKELANGID(LANG_NEUTRAL, SUBLANG_RUSSIAN_RUSSIA),
+				  (LPSTR)&szBuffer,
+				  0,
+				  NULL
+	);
+	return szBuffer;
+}
+VOID PrintLastError(DWORD dwMessageID)
+{
+	LPSTR szMessage = FormatLastError(dwMessageID);
+	printf("Error %i:%s", dwMessageID, szMessage);
+	LocalFree(szMessage);
+}
