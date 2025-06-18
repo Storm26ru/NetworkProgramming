a7a43c5ff9e3b795af7eb9915dd82953ca5e7266
Storm <Storm@STORMSSD>
2025-06-19 00:09:16 +0300

    client done

--- WinSockClient/main.cpp
+++ WinSockClient/main.cpp
@@ -12,10 +12,12 @@ using namespace std;
 
 
 #define DEFAULT_PORT "27015"
+#define DEFAULT_BUFFER_LENGTN 1500
 
 void main()
 {
 	setlocale(LC_ALL, "");
+	cout << "WinSock Client" << endl;
 	
 	WSADATA wsaData;
 	INT iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -47,5 +49,42 @@ void main()
 		WSACleanup();
 		return;
 	}
+
+	iResult = connect(connect_socket, result->ai_addr, result->ai_addrlen);
+	if (iResult)
+	{
+		PrintLastError(WSAGetLastError());
+		closesocket(connect_socket);
+		freeaddrinfo(result);
+		WSACleanup();
+		return;
+	}
+	
+	CONST CHAR sendbuffer[] = "Hellow Server, I am client ";
+	CHAR recvbuffer[DEFAULT_BUFFER_LENGTN]{};
+	iResult = send(connect_socket, sendbuffer, sizeof(sendbuffer), 0);
+	if (iResult == SOCKET_ERROR)
+	{
+		PrintLastError(WSAGetLastError());
+		closesocket(connect_socket);
+		freeaddrinfo(result);
+		WSACleanup();
+		return;
+	}
+	do
+	{
+		iResult = recv(connect_socket, recvbuffer, DEFAULT_BUFFER_LENGTN, 0);
+		if (iResult > 0)cout << "Receved bytes: " << iResult << " Message: " << recvbuffer << endl;
+		else if (iResult == 0)cout << "Connection closing" << endl;
+		else PrintLastError(WSAGetLastError());
+	} while (iResult > 0);
+
+	iResult = shutdown(connect_socket, SD_SEND);
+	if (iResult == SOCKET_ERROR) PrintLastError(WSAGetLastError());
+	closesocket(connect_socket);
+	freeaddrinfo(result);
+	WSACleanup();
+
+
 
 }
