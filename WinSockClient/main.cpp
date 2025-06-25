03d747c7912fdc7f568facae2d63b575ff466abf
Storm <Storm@STORMSSD>
2025-06-25 23:10:50 +0300

    Client thread start

--- WinSockClient/main.cpp
+++ WinSockClient/main.cpp
@@ -1,4 +1,10 @@
-﻿#include <WinSock2.h>
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include<Windows.h>
+#include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <stdio.h>
 #include <iostream>
@@ -14,6 +20,10 @@ using namespace std;
 #define DEFAULT_PORT "27015"
 #define DEFAULT_BUFFER_LENGTN 1500
 #define SORY "Connection is currently unavailable, please try again later."
+
+VOID Recv(SOCKET connect_socket);
+VOID Send(SOCKET connect_socket);
+
 
 void main()
 {
@@ -61,9 +71,9 @@ void main()
 		return;
 	}
 
-	CHAR sendbuffer[DEFAULT_BUFFER_LENGTN]= "Hello Server, I am client";
-	CHAR recvbuffer[DEFAULT_BUFFER_LENGTN]{};
-	iResult = send(connect_socket, sendbuffer, strlen(sendbuffer), 0);
+	
+	//CHAR recvbuffer[DEFAULT_BUFFER_LENGTN]{};
+	/*iResult = send(connect_socket, sendbuffer, strlen(sendbuffer), 0);
 	if (iResult == SOCKET_ERROR)
 	{
 		PrintLastError(WSAGetLastError());
@@ -71,22 +81,10 @@ void main()
 		freeaddrinfo(result);
 		WSACleanup();
 		return;
-	}
-	do
-	{
-		ZeroMemory(recvbuffer, DEFAULT_BUFFER_LENGTN);
-		ZeroMemory(sendbuffer, DEFAULT_BUFFER_LENGTN);
-		iResult = recv(connect_socket, recvbuffer, DEFAULT_BUFFER_LENGTN, 0);
-		if (iResult > 0)
-		{
-			cout << "Receved bytes: " << iResult << " Message: " << recvbuffer << endl;
-			cout << "Введите сообщение: ";
-			cin.getline(sendbuffer, DEFAULT_BUFFER_LENGTN);
-			if (send(connect_socket, sendbuffer, strlen(sendbuffer), 0) == SOCKET_ERROR) PrintLastError(WSAGetLastError());
-		}
-		else if (iResult == 0)cout << "Connection closing" << endl;
-		else PrintLastError(WSAGetLastError());
-	} while (iResult > 0);
+	}*/
+	Recv(connect_socket);
+	HANDLE hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Send, (LPVOID)connect_socket, 0, NULL);
+
 
 	iResult = shutdown(connect_socket, SD_SEND);
 	if (iResult == SOCKET_ERROR) PrintLastError(WSAGetLastError());
@@ -97,4 +95,51 @@ void main()
 
 
 
+}
+VOID Recv(SOCKET connect_socket)
+{
+	CHAR recvbuffer[DEFAULT_BUFFER_LENGTN]{};
+	INT iResult = 0;
+	do
+	{
+		iResult = recv(connect_socket, recvbuffer, DEFAULT_BUFFER_LENGTN, 0);
+		if (iResult > 0)
+		{
+			cout << "Receved bytes: " << iResult << " Message: " << recvbuffer << endl;
+		}
+		else if (iResult == 0)cout << "Connection closing" << endl;
+		else PrintLastError(WSAGetLastError());
+		ZeroMemory(recvbuffer, DEFAULT_BUFFER_LENGTN);
+	} while (iResult > 0);
+}
+VOID Send(SOCKET connect_socket)
+{
+	INT iResult = 0;
+	CHAR sendbuffer[DEFAULT_BUFFER_LENGTN] = "Hello Server, I am client";
+	do
+	{
+		//ZeroMemory(recvbuffer, DEFAULT_BUFFER_LENGTN);
+		//iResult = recv(connect_socket, recvbuffer, DEFAULT_BUFFER_LENGTN, 0);
+		iResult = send(connect_socket, sendbuffer, strlen(sendbuffer), 0);
+		//if (iResult == SOCKET_ERROR)
+		//{
+		//	/*cout << "Receved bytes: " << iResult << " Message: " << recvbuffer << endl;
+		//	cout << "Введите сообщение: ";
+		//	cin.getline(sendbuffer, DEFAULT_BUFFER_LENGTN);
+		//	if (send(connect_socket, sendbuffer, strlen(sendbuffer), 0) == SOCKET_ERROR) PrintLastError(WSAGetLastError());*/
+		//	PrintLastError(WSAGetLastError());
+		//	closesocket(connect_socket);
+		//	freeaddrinfo(result);
+		//	WSACleanup();
+		//	return;
+		//}
+		cout << iResult << " Byte sent" << endl;
+		ZeroMemory(sendbuffer, DEFAULT_BUFFER_LENGTN);
+		cout << "Введите сообщение: ";
+		SetConsoleCP(1251);
+		cin.getline(sendbuffer, DEFAULT_BUFFER_LENGTN);
+		SetConsoleCP(866);
+		//else if (iResult == 0)cout << "Connection closing" << endl;
+		//else PrintLastError(WSAGetLastError());
+	} while (iResult > 0);
 }
